de544ff2f9662bd0e4386c515339089f7e2ac292
SixSense <33900575+SixSen@users.noreply.github.com>
2019-09-20 09:47:12 +0800

    更新逻辑

--- app/home/views.py
+++ app/home/views.py
@@ -320,7 +320,7 @@ def msg():
         tags = list(map(int, data))
         conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='1232123', db='xinfang')
         cursor = conn.cursor()
-        sql = "DELETE FROM `tag` WHERE `letter`=%s;" % (key)
+        sql = "DELETE FROM `tag` WHERE `letter`=%s;" % key
         cursor.execute(sql)
         conn.commit()
         for tag in tags:
